ae049edc60651b99f40036fbec07ebb688842ca0
Josh Justice <me@codingitwrong.com>
2022-11-25 11:35:40 -0500

    Remove references to models--don't work well with migrations

--- app/todo_repo.rb
+++ app/todo_repo.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 require_relative '../config/sequel'
-# require_relative 'todo'
 
 module TodoApp
   class TodoRepo
     def all
-      # Todo.all.map(&:values)
       DB[:todos].all
     end
   end
